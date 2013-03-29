e6bf854f8a2589f74c7b356516c92033c99c32b8
tobyw4n <tobycollier@gmail.com>
2013-03-29 14:49:12 -0400

    first run at rules

---
+++ spec/defines/rule_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'pam_ssh_agent_auth_sudo::rule' do
+
+  let(:title) { 'pam_ssh_agent_auth_sudo'}
+  
+  it do
+    should include_class('pam_ssh_agent_auth_sudo')
+  end
+
+  it do
+    should contain_file('/etc/pam.d/sudo').with({
+      'ensure'  => 'present',
+      'owner'   => 'root',
+      'group'   => 'root',
+      'mode'    => '0644',
+      'source'  => 'puppet:///modules/pam_ssh_agent_auth_sudo/sudo',
+      'require' => 'Package[$package]',
+    })
+  end
+
+end

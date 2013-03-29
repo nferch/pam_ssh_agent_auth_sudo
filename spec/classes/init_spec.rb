4e3cc196c21729dbf3c7e4aeb53205f50077ede7
tobyw4n <tobycollier@gmail.com>
2013-03-29 16:15:54 -0400

    finally a working rake spec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,22 +1,23 @@
 require 'spec_helper'
 
 describe 'pam_ssh_agent_auth_sudo' do
-
-  let(:title) { 'pam_ssh_agent_auth_sudo'}
   
   it do
     should include_class('pam_ssh_agent_auth_sudo')
   end
 
-  it do
-    should contain_file('/etc/pam.d/sudo').with({
-      'ensure'  => 'present',
-      'owner'   => 'root',
-      'group'   => 'root',
-      'mode'    => '0644',
-      'source'  => 'puppet:///modules/pam_ssh_agent_auth_sudo/sudo',
-      'require' => 'Package[$package]',
-    })
+  context 'with operatingsystem amazon' do
+    let(:facts) { { :operatingsystem => 'amazon' } }
+    it do
+      should contain_file('/etc/pam.d/sudo').with({
+        'ensure'  => 'present',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+        'source'  => 'puppet:///modules/pam_ssh_agent_auth_sudo/sudo',
+        'require' => 'Package[pam_ssh_agent_auth]'
+      })
+    end
   end
 
 end
